9094079a2cf823dfeb92161481ecf03315900c67
Daniel Oliva <oliva.gadan21@triana.salesianos.edu>
2020-12-16 10:30:42 +0100

    Primer commit

---
+++ Proyecto_Java/src/com/entornosdesarollo/AppJava.java
@@ -0,0 +1,12 @@
+package com.entornosdesarollo;
+
+public class AppJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HOLA MUNDO");
+
+	}
+
+}

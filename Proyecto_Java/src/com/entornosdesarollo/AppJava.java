8d43b8ffaa85b044114fce91090022cbb94464ef
Daniel Oliva <oliva.gadan21@triana.salesianos.edu>
2020-12-16 10:46:01 +0100

    Primer commiteo x2

--- Proyecto_Java/src/com/entornosdesarollo/AppJava.java
+++ Proyecto_Java/src/com/entornosdesarollo/AppJava.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AppJava {
 
 		
-		public static String leerStringPorConsola( ) {
+	public static String leerStringPorConsola( ) {
 			Scanner sc = new Scanner(System.in);
 			String result=sc.next();
 			sc.close();

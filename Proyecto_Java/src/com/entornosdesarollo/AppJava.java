0b0e1bcf4355c05d0c3531f1d06db5a5784eb974
Daniel Oliva <oliva.gadan21@triana.salesianos.edu>
2020-12-16 10:56:47 +0100

    Saludo personalizado

--- Proyecto_Java/src/com/entornosdesarollo/AppJava.java
+++ Proyecto_Java/src/com/entornosdesarollo/AppJava.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class AppJava {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-	public static String leerStringPorConsola( ) {
+		System.out.println("Introduce tu nombre");
+		String nombre= leerStringPorConsola();
+		System.out.println("Muy buenas, " + nombre);
+		
+	}
+		
+	public static String leerStringPorConsola() {
 			Scanner sc = new Scanner(System.in);
 			String result=sc.next();
 			sc.close();
 			return result;
-		
 	}
 
-}
+	}
 
 

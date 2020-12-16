f7a23d48fe368f9819890bc4f815f093fc7c8f05
Daniel Oliva <oliva.gadan21@triana.salesianos.edu>
2020-12-16 10:43:25 +0100

    Leer por teclado

--- Proyecto_Java/src/com/entornosdesarollo/AppJava.java
+++ Proyecto_Java/src/com/entornosdesarollo/AppJava.java
@@ -1,12 +1,18 @@
 package com.entornosdesarollo;
 
+import java.util.Scanner;
+
 public class AppJava {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("HOLA MUNDO");
-
+		public static String leerStringPorConsola( ) {
+			Scanner sc = new Scanner(System.in);
+			String result=sc.next();
+			sc.close();
+			return result;
+		
 	}
 
 }
+
+
